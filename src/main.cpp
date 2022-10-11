a4b9c61772098da6daf86c0a4439d42827702ff1
Jakub Sadowski <github@vcc.earth>
2022-10-11 23:18:51 +0200

    add POC buttons, make project more compatible with cookiecutter

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,13 @@
 #include "stripper.h"
 #include "elceder.h"
 
+#include "commonFwUtils.h"
+
 Button2 button;
 
 void pressCB(Button2& btn) {
   Serial.println("1");
 }
-
 void spawn_tasks();
 
 void setup()
@@ -26,14 +27,7 @@ void setup()
 }
 
 void loop(){
-  static int test = 0;
-  static uint8_t heartbeat_pattern[] = {1,0,0,1,0,0,0,0,0,0,0,0,0};
-
-  // elceder_fill_row(1,"test %d",test++);
-  
-  uint8_t pattern_index = (test++)% sizeof(heartbeat_pattern);
-  digitalWrite(PIN_LED, heartbeat_pattern[pattern_index]);
-
+  handle_io_pattern(PIN_LED,PATTERN_HBEAT);
   vTaskDelay(150);
 }
 
@@ -69,9 +63,16 @@ void serial_read_task(void* params){
   }
 }
 
-const float aver_ratio = 100.0f;
+const float aver_ratio = 1000.0f;
 const float addition_factor = 1.0f/aver_ratio;
 const float sum_factor = 1.0f-addition_factor;
+
+// the value read from cap is in range 0-100
+// for example let it be 80
+// by trial and error, touching a cap button
+// increases the read value by about 10%
+// so I assume 5% increase for now.
+const float trigger_factor = 0.05f;
 
 const int cbuts[3] = {33, 27, 14};
 
@@ -80,33 +81,56 @@ void touch_task (void* params){
   float meases[3] = {0,0,0};
   uint32_t counter = 0;
 
+  bool button_pressed[3] = {0,0,0};
+
   while (true){
+    vTaskSuspendAll(); //block task switch cause it messes with touchRead
+
     for (int i=0;i<3;i++){
       meases[i] = (float)touchRead(cbuts[i]);
 
-      accs[i] = 
-        accs[i]*sum_factor + 
-        meases[i]*addition_factor;
+      //update averaging buffers
+      if (accs[i] < 30){
+        //on first run skip averaging
+        accs[i] = meases[i];
+      } else {
+        accs[i] = 
+          accs[i]*sum_factor + 
+          meases[i]*addition_factor;
+      }
+
+      float delta_touch = accs[i]-meases[i];
+      if (delta_touch > (accs[i] * trigger_factor)){
+        if (button_pressed[i] == false){
+          button_pressed[i] = true;
+          elceder_fill_row(0,"But %d press!",i);
+        }
+      } else {
+        button_pressed[i] = false;
+      }
     }
 
-    elceder_fill_row(
-      0,"%03d/%03d/%03d/%04d",
-      (int)meases[0],
-      (int)meases[1],
-      (int)meases[2],
-      counter%10000
-    );
+    // elceder_fill_row(
+    //   0,"%03d/%03d/%03d/%04d",
+    //   (int)meases[0],
+    //   (int)meases[1],
+    //   (int)meases[2],
+    //   counter%10000
+    // );
       
-    elceder_fill_row(
-      1,"%03d/%03d/%03d       ",
-      (int)(accs[0]-meases[0]),
-      (int)(accs[1]-meases[1]),
-      (int)(accs[2]-meases[2])
-    );
+    // elceder_fill_row(
+    //   0,"%03d/%03d/%03d       ",
+    //   (int)(accs[0]-meases[0]),
+    //   (int)(accs[1]-meases[1]),
+    //   (int)(accs[2]-meases[2])
+    // );
     counter++;
+    xTaskResumeAll();
     vTaskDelay(100);
   }
 }
+
+void diagnostics_task(void * parameter);
 
 void spawn_tasks(){
   xTaskCreate(
@@ -127,14 +151,14 @@ void spawn_tasks(){
     NULL
   );
 
-  // xTaskCreate(
-  //   wifi_task,
-  //   "wifi task",
-  //   10000,
-  //   NULL,
-  //   3,
-  //   NULL
-  // );
+  xTaskCreate(
+    wifi_task,
+    "wifi task",
+    10000,
+    NULL,
+    3,
+    NULL
+  );
   
   // xTaskCreate(
   //   serial_read_task,
@@ -154,12 +178,20 @@ xTaskCreate(
     NULL
   );
 
+xTaskCreate(
+    diagnostics_task,
+    "diag task",
+    10000,
+    NULL,
+    3,
+    NULL
+  );
+
 }
 
 
 //this will print CPU usage stats at some point
 
-/*
 void diagnostics_task(void * parameter){
   char diag_buff[256];
 
@@ -169,12 +201,13 @@ void diagnostics_task(void * parameter){
   xLastWakeTime = xTaskGetTickCount();
 
   while(1){
-    vTaskGetRunTimeStats((char*)&diag_buff);
-    //Serial.println((char*)&diag_buff);
-    Serial.println("test");
+    // vTaskGetRunTimeStats((char*)&diag_buff);
+    // //Serial.println((char*)&diag_buff);
+    // Serial.println("test");
+
+    // Serial.printf( "%d\n\r", (int)uxTaskGetStackHighWaterMark( NULL ));
 
     vTaskDelayUntil(&xLastWakeTime,xFrequency);
 
   }
 }
-*/
---
+++ src/whoisUtils.cpp
@@ -0,0 +1,117 @@
+//To be dealt with later
+// #include <HTTPClient.h>
+// WiFiClient client;
+
+/*
+void loop()
+{
+  if(WiFi.status() != WL_CONNECTED) connect();
+
+  if (millis() - lastRequest >= 60 * 1000)
+  {
+    json = getWhois();
+    lastRequest = millis();
+  }
+
+  static int counter = 0;
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("People: ");
+  lcd.print(json["headcount"].as<long>());
+  if (json["users"].size() > 0)
+  {
+    int index = (counter++) % (json["users"].size());
+    lcd.setCursor(0, 1);
+    lcd.print(json["users"][index].as<String>());
+  }
+  else
+  {
+    lcd.setCursor(0, 1);
+    lcd.print("Pool's Closed");
+  }
+  delay(1000);
+}
+
+*/
+/*
+StaticJsonDocument<512> getWhois()
+{
+  StaticJsonDocument<512> json;
+  DeserializationError error = deserializeJson(json, requestWhois());
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Parsing");
+  if (error != DeserializationError::Ok)
+  {
+    lcd.setCursor(0, 1);
+    lcd.print(error.c_str());
+    delay(1000);
+  }
+  return json;
+}
+*/
+
+
+/*
+String requestWhois()
+{
+  String payload = "";
+  const char *root_ca = \
+    "  -----BEGIN CERTIFICATE-----\n"
+    "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw\n"
+    "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n"
+    "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4\n"
+    "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu\n"
+    "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY\n"
+    "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc\n"
+    "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+\n"
+    "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U\n"
+    "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW\n"
+    "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH\n"
+    "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC\n"
+    "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv\n"
+    "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn\n"
+    "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn\n"
+    "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw\n"
+    "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI\n"
+    "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV\n"
+    "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq\n"
+    "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL\n"
+    "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ\n"
+    "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK\n"
+    "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5\n"
+    "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur\n"
+    "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC\n"
+    "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc\n"
+    "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq\n"
+    "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA\n"
+    "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d\n"
+    "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=\n"
+    "-----END CERTIFICATE-----";
+
+  HTTPClient http;
+  http.begin("https://whois.at.hsp.sh/api/now", root_ca); //Specify the URL and certificate
+  int httpCode = http.GET();                              //Make the request
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Requesting");
+  if (httpCode > 0)
+  { //Check for the returning code
+
+    payload = http.getString();
+    http.end(); //Free the resources
+    lcd.setCursor(0, 1);
+    lcd.print(httpCode + " " + payload);
+  }
+  else
+  {
+    Serial.println(F("Error on HTTP request"));
+    lcd.print(httpCode + " error");
+    delay(1000);
+    return "";
+  }
+  http.end(); //Free the resources
+  return payload;
+}*/
