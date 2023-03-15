757fb95c3e626b23cf1769fb2961b4e4a425734d
Jakub Sadowski <github@vcc.earth>
2023-03-15 22:00:10 +0100

    fix the way arduino is called from espidf

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,6 @@
 #include "netUtils.h"
 #include "commonFwUtils.h"
 
-// force C linkage for app_main()
-extern "C" {
-	void app_main(void);
-}
-
-void app_main(){
-  setup();
-  for(;;){
-    loop();
-  }
-}
-
 Button2 button;
 
 void pressCB(Button2& btn) {
